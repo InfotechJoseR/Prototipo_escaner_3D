9f31d10716ea21b30ec0d6c06c54f092c761274b
José Ruiz <jose96ph@gmail.com>
2024-09-11 07:58:01 -0500

    Create SISTEMA_DE_CONTROL.ino
    Creacion sistema de control

---
+++ PROTOTIPO ESCANER 3D/SISTEMA DE CONTROL/SISTEMA_DE_CONTROL/SISTEMA_DE_CONTROL.ino
@@ -0,0 +1,5 @@
+// Librerías utilizadas
+#include <LiquidCrystal.h>  // Incluye la librería para el LCD estándar (no I2C)
+#include <Wire.h>  // Incluye la librería para el I2C del LCD. SCL se conecta al pin A5, SDA se conecta al pin A4. Se necesita un jumper en el pin de retroiluminación LED en la placa I2C.
+#include <Stepper.h>  // Incluye la librería para controlar motores paso a paso.
+#include <Servo.h>  // Incluye la librería para controlar servos.
