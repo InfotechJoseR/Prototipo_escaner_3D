082a72005eb455089e4aa61d94b2af27c23ed711
José Ruiz <jose96ph@gmail.com>
2024-09-12 19:16:26 -0500

    Update Prototipo.py
    Directorios de trabajo

--- PROTOTIPO ESCANER 3D/SOFTWARE/Prototipo.py
+++ PROTOTIPO ESCANER 3D/SOFTWARE/Prototipo.py
@@ -14,3 +14,21 @@ import customtkinter
 
 # Ruta completa del ejecutable de COLMAP
 COLMAP_PATH = "C:/Users/José Ruiz/Documents/ESPE/TRABAJO UIC 202450/DESARROLLO DEL PROTOTIPO DE ESCANER 3D/5. SOFTWARE DEL PROTOTIPO/SOFTWARE PROTOTIPO DE ESCANER 3D/Protipo_escaner_3D/colmap-x64-windows-nocuda/bin/colmap.exe"
+
+# Directorios de trabajo
+BASE_DIR = os.getcwd()
+IMAGE_DIR = os.path.join(BASE_DIR, 'images')
+DATABASE_PATH = os.path.join(BASE_DIR, 'database.db')
+SPARSE_DIR = os.path.join(BASE_DIR, 'sparse')
+DENSE_DIR = os.path.join(BASE_DIR, 'dense')
+MODEL_DIR = os.path.join(BASE_DIR, 'model.ply')
+MESH_DIR = os.path.join(BASE_DIR, 'mesh.ply')
+
+# Función para asegurar que los directorios existen
+def ensure_directory_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+# Asegurarse de que los directorios de salida existen
+ensure_directory_exists(SPARSE_DIR)
+ensure_directory_exists(DENSE_DIR)
